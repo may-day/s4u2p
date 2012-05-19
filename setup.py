f960ed5bbf82660cf316b7d2796ec9433505be8d
Norman Krämer <kraemer.norman@googlemail.com>
2012-05-19 22:42:21 +0200

    added MANIFEST.in and modded setup.py to build sdist

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 # -*- coding: utf8 -*-
 from distutils.core import setup, Extension
-import sys
 import commands
 
 long_description = """
 This is an small extension to the python kerberos package.
 It adds support for "services for user to self/proxy" (s4u2s/s4u2p) kerberos functionality.
+It adds authGSSImpersonation* functions that let you (as a server) act on behalf of a user.
+No actual delegated ticket from a user needs to be forwarded to the server component.
+Instead the server account needs to be set up as described here:
+http://k5wiki.kerberos.org/wiki/Manual_Testing#Services4User_testing
+
+For some overview of s4u see this:
+http://msdn.microsoft.com/en-us/magazine/cc188757.aspx#S2
+
 """
 
 setup (
@@ -15,12 +22,18 @@ setup (
     author_email="kraemer.norman@googlemail.com",
     description = "Kerberos high-level interface to the s4u kerberos functionality",
     long_description=long_description,
+    url="https://github.com/may-day/s4u2p",
+    download_url = "https://github.com/may-day/s4u2p/tarball/0.1",
     classifiers = [
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 2",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Systems Administration :: Authentication/Directory"
         ],
+    license = "Apache License, Version 2.0",
     ext_modules = [
         Extension(
             "s4u2p",
